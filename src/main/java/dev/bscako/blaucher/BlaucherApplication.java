2c49dfaf6ec1e8fab913e5c0b8d764d38dcd1681
SAMBIENOU K. Caleb <dev.bscako@gmail.com>
2019-02-26 14:51:29 +0300

    INITIALISATION Application

---
+++ src/main/java/dev/bscako/blaucher/BlaucherApplication.java
@@ -0,0 +1,13 @@
+package dev.bscako.blaucher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlaucherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlaucherApplication.class, args);
+    }
+
+}
